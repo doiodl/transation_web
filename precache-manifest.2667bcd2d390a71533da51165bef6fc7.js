fabe46a0939d27349a95573184fb518a62881324
Тимур Зайнуллин <timurzajnullin@MacBook-Pro-Timur.local>
2019-11-03 17:31:02 +0300

    Updates

---
+++ precache-manifest.2667bcd2d390a71533da51165bef6fc7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b19113f173a9eae4c0e0e8d3d00571d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a292695aac04aa65850",
+    "url": "/static/css/2.27cde75d.chunk.css"
+  },
+  {
+    "revision": "e8f10f675b080c98c092",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "0a292695aac04aa65850",
+    "url": "/static/js/2.fcb8b9bb.chunk.js"
+  },
+  {
+    "revision": "e8f10f675b080c98c092",
+    "url": "/static/js/main.d059785d.chunk.js"
+  },
+  {
+    "revision": "18d847caf70e97c88d42",
+    "url": "/static/js/runtime-main.02cfa1ef.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
