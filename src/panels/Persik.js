72375baf61d2a337e67cb3a48dc3121256b533f4
Тимур Зайнуллин <timurzajnullin@MacBook-Pro-Timur.local>
2019-11-29 22:01:23 +0300

    g

--- src/panels/Persik.js
+++ src/panels/Persik.js
@@ -19,7 +19,6 @@ import './Persik.css';
 const osName = platform();
 
 const Persik = props => {
-	connect.send("VKWebAppResizeWindow", {"width": 'device-width', "height": 940});
 	function send_req() {
 		let user = {
 			type: 'js_test',
