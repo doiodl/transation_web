b8b48e953e7336b53253fa9b103e5837d4d6604f
Branden Theia <btheia@ox-c1.21-school.ru>
2019-12-13 19:05:33 +0300

    g

---
+++ src/panels/Chat.js
@@ -0,0 +1,45 @@
+import React, { Component} from 'react';
+import PropTypes from 'prop-types';
+import connect from '@vkontakte/vk-connect';
+import { platform, IOS } from '@vkontakte/vkui';
+import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
+import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
+import HeaderButton from '@vkontakte/vkui/dist/components/HeaderButton/HeaderButton';
+import Group from '@vkontakte/vkui/dist/components/Group/Group';
+import Div from '@vkontakte/vkui/dist/components/Div/Div';
+import Button from '@vkontakte/vkui/dist/components/Button/Button';
+import Icon28ChevronBack from '@vkontakte/icons/dist/28/chevron_back';
+import Icon24Back from '@vkontakte/icons/dist/24/back';
+import $ from 'jquery';
+import persik from '../img/persik.png';
+import 'youtube-video-js';
+import YouTubePlayer from 'react-player/lib/players/YouTube';
+import './Persik.css';
+
+class Chat extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {massive: "", cur_n: ""};
+    }
+
+    update()
+    {
+        
+    }
+    render(){
+        const test = ["ololo", "test"];
+        return (
+
+              <div name="chats">
+                {this.state.massive.forEach((element) =>{
+                    return <span> {element} </span>;
+                })}
+              </div>
+    
+        );
+    }
+    
+
+}
+
+export default Persik;
